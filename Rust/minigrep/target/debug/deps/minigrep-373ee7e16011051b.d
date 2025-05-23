25eede8ead5078c28bb5684962efaf34fbc4710f
Maks <csgo20041@gmail.com>
2025-05-23 09:19:13 +0200

    add: RUST_minigrep

---
+++ Rust/minigrep/target/debug/deps/minigrep-373ee7e16011051b.d
@@ -0,0 +1,5 @@
+/home/maks/Programming/RandomThings/Rust/minigrep/target/debug/deps/minigrep-373ee7e16011051b: src/main.rs
+
+/home/maks/Programming/RandomThings/Rust/minigrep/target/debug/deps/minigrep-373ee7e16011051b.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/maks/Programming/RandomThings/Rust/minigrep/target/debug/minigrep: /home/maks/Programming/RandomThings/Rust/minigrep/src/lib.rs /home/maks/Programming/RandomThings/Rust/minigrep/src/main.rs
